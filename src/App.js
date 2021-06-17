1efb8215231bd53405d9b3dd935d870b25c13826
OwenShuaiLiu <owen.shuai.liu@berkeley.edu>
2021-06-17 13:19:28 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+import Projects from './Projects';
+import SocialProfiles from './SocialProfiles';
+import profile from './assets/profile.png';
+ 
+class App extends Component{
+ 
+   state = {displayBio: false};
+ 
+   toggleDisplayBio = () => {
+       this.setState({displayBio: !this.state.displayBio});
+   }
+ 
+   render() {
+       return (
+           <div>
+               <img src={profile} alt='profile' className="profile"/>
+               <h1>Hello!</h1>
+               <p>My name is Owen. I'm a system analyst.</p>
+               <p>I'm always looking forward to working on meaningful projects.</p>
+               {
+                   this.state.displayBio? (
+                       <div>
+                           <p>I live in Mountain View, practice every day.</p>
+                           <p>My favourite language is Python, and I think React.js is asso awesome.</p>
+                           <p>Beside practicing, I also love sports and reading.</p>
+                           <button onClick={this.toggleDisplayBio}>Show less</button>
+                       </div>
+                   ) : (
+                       <div>
+                           <button onClick={this.toggleDisplayBio}>Read more</button>
+                       </div>
+                   )
+               }
+               <hr />
+               <Projects />
+               <hr />
+               <SocialProfiles />
+           </div>
+       )
+   }
 }
-
+ 
 export default App;
